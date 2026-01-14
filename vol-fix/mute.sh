8448d6bf6e270f9ea9fe8b03df19d679fcfc4fa7
Branden Palmer <brandenpalmer08@gmail.com>
2026-01-13 22:46:56 -0600

    add vol fixes for g14

---
+++ vol-fix/mute.sh
@@ -0,0 +1,4 @@
+
+#!/usr/bin/env bash
+amixer -q -c 2 sset Master 100%
+amixer -q -c 2 sset PCM toggle
---
+++ vol-fix/up.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+amixer -q -c 2 sset Master 100%
+amixer -q -c 2 sset PCM 1%+
