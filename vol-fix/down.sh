efa1c8513e1fa4dfd3a7ad80eed911839f678748
Branden Palmer <brandenpalmer08@gmail.com>
2026-01-13 22:56:24 -0600

    adj vol fixes

--- vol-fix/down.sh
+++ vol-fix/down.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+# /home/branden/dotfiles/vol-fix/down.sh
 amixer -q -c 2 sset Master 100%
-amixer -q -c 2 sset PCM 1%-
+amixer -q -c 2 sset Speaker 100%
+amixer -q -c 2 sset PCM 5%-
--- vol-fix/mute.sh
+++
@@ -1,4 +0,0 @@
-
-#!/usr/bin/env bash
-amixer -q -c 2 sset Master 100%
-amixer -q -c 2 sset PCM toggle
